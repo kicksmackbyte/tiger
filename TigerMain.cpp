d8d78fff354bd4f009ed79e73edba58d06683db3
Alberto Garcia <kicksmackbyte@gmail.com>
2020-08-29 11:41:37 -0700

    Add main program

---
+++ TigerMain.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <fstream>
+
+#include "antlr4-runtime.h"
+#include "TigerLexer.h"
+#include "TigerParser.h"
+
+using namespace antlrcpp;
+using namespace antlr4;
+
+int main(int argc, const char* args[])
+{
+    std::ifstream ins;
+    ins.open(args[1]);
+
+    ANTLRInputStream input(ins);
+
+    TigerLexer lexer(&input);
+    CommonTokenStream tokens(&lexer);
+
+    std::cout << "Tokens: " << std::endl;
+    tokens.fill();
+
+    for(auto token : tokens.getTokens())
+    {
+        std::cout << token->toString() << std::endl;
+    }
+
+    TigerParser parser(&tokens);
+    tree::ParseTree* tree = parser.program();
+
+    std::cout << "Parse tree (Lisp format): " << std::endl;
+    std::cout << tree->toStringTree(&parser) << std::endl;
+
+    return 0;
+}
