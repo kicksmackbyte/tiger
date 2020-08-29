f81779411c0aaa24c00ba4d5e64871947d15052e
Alberto Garcia <kicksmackbyte@gmail.com>
2020-08-29 10:41:53 -0700

    Specify output directory for antlr4

--- devenv.sh
+++ devenv.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 export CLASSPATH=".:/home/twice/tiger/antlr-4.7.2-complete.jar:$CLASSPATH"
-alias antlr4="java -Xmx1g -cp ./antlr-4.7.2-complete.jar:. org.antlr.v4.Tool -Dlanguage=Cpp -no-listener -visitor"
+alias antlr4="java -Xmx1g -cp ./antlr-4.7.2-complete.jar:. org.antlr.v4.Tool -Dlanguage=Cpp -no-listener -visitor -o target"
 alias grun="java org.antlr.v4.gui.TestRig"
